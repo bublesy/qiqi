40629d99774544419886a4efe53c7f66fc09942d
tkless <accounts_5f041221b23b8d003696b26a@mail.teambition.com>
2020-09-10 17:07:11 +0800

    合并 #173847 从 sun 到 master 1
    https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton/merge_request/173847  
    
    squash 合并:
    
    * 采购

---
+++ vue-admin/src/views/purchase/purchase-not-included-overdue/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <el-container>
+    <el-main>
+      <div>
+        <el-header align="center">采购过期未进一览表</el-header>
+        <el-form :inline="true" :model="form" size="mini" align="center">
+          <el-form-item label="制表人:">
+            <el-input v-model="form.identity" />
+          </el-form-item>
+          <el-form-item label="日期" prop="deliveryTime">
+            <el-date-picker
+              v-model="form.time"
+              align="right"
+              type="date"
+              placeholder="选择日期"
+            />
+          </el-form-item>
+        </el-form>
+        <el-table
+          ref="multipleTable"
+          :summary-method="getSummaries"
+          :data="tableData"
+          stripe
+          border
+          highlight-current-row
+          style="width: 100%"
+        >
+          <el-table-column width="50px" align="center" />
+          <el-table-column prop="taskNumber" label="任务编号" />
+          <el-table-column prop="customerName" label="客户名称" />
+          <el-table-column prop="paperSize" label="纸片尺寸(MM)纸长 X 纸宽">
+            <template slot-scope="scope">
+              {{ scope.row.paperLength }}{{ scope.row.paperWidth }}
+            </template>
+          </el-table-column>
+          <el-table-column v-show="true" prop="orderQuantity" label="订单数量" width="140" />
+          <el-table-column prop="purQuantity" label="购入数量" />
+          <el-table-column prop="quaNotEnt" label="未进数量" />
+          <el-table-column prop="supplier" label="供方" />
+          <el-table-column prop="orderDate" label="订货日期" />
+          <el-table-column prop="purchaseDate" label="进货日期" />
+        </el-table>
+        <el-pagination
+          background
+          layout="total, sizes, prev, pager, next"
+          :total="pagination.total"
+          align="center"
+          :current-page="pagination.page"
+          :page-size="pagination.size"
+          @size-change="sizeChange"
+          @current-change="pageChange"
+        />
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script scope>
+import initData from '@/mixins/initData'
+export default {
+  name: 'PurchaseNotIncludedOverdue',
+  mixins: [initData],
+  data() {
+    return {
+      tableData: [{
+        taskNumber: '1'
+      }],
+      form: {}
+    }
+  },
+  methods: {
+    /**
+     * 表格合计
+     */
+    getSummaries(param) {
+      const { columns, data } = param
+      const sums = []
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '总计'
+          return
+        }
+        if (index === 2 || index === 3 || index === 4 || index === 5) {
+          const values = data.map(item => Number(item[column.property]))
+          if (!values.every(value => isNaN(value))) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr)
+              if (!isNaN(value)) {
+                return prev + curr
+              } else {
+                return prev
+              }
+            }, 0)
+            sums[index] = parseFloat(sums[index] / 100).toFixed(2)
+          } else {
+            sums[index] = ''
+          }
+        }
+      })
+      return sums
+    }
+  }
+}
+</script>
+
+<style scoped>
+i {
+  cursor: pointer;
+}
+</style>
