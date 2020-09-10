cc09aaebeb6157500270af071cbd94647403a533
18368673570 <419009863@qq.com>
2020-09-10 14:08:49 +0800

    新建基本资料文件夹

---
+++ vue-admin/src/views/order/daily/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div style="margin:30px">
+    <p class="font">客户订单日报表</p>
+    <span style="margin-left:10%">制表:</span>
+    <span style="margin-left:400px">打印日期:</span>
+    <el-table
+      ref="multipleTable"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55" />
+      <el-table-column prop="name" label="客户名称" width="120" />
+      <el-table-column prop="name" label="任务编号" width="120" />
+      <el-table-column prop="name" label="款号" width="120" />
+      <el-table-column prop="name" label="材质" width="120" />
+      <el-table-column prop="name" label="订单尺寸" width="120" />
+      <el-table-column prop="name" label="订单数量" width="120" />
+      <el-table-column prop="name" label="单价" width="120" />
+      <el-table-column prop="name" label="金额" width="120" />
+      <el-table-column prop="name" label="交货日期" width="120" />
+    </el-table>
+    <el-pagination
+      :current-page="page"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Daily',
+  data() {
+    return {
+      tableData: [],
+      page: 1,
+      size: 10,
+      total: 0
+    }
+  },
+  methods: {
+    initTable() {},
+    handleSelectionChange() {},
+    handleSizeChange(size) {
+      this.size = size
+      this.initTable()
+    },
+    handleCurrentChange(page) {
+      this.page = page
+      this.initTable()
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.font{
+  font-weight: bold;
+  width:400px;
+  height: 100px;
+  line-height: 100px;
+  font-size: 30px;
+  font-family: 'Courier New', Courier, monospace;
+  /* text-align: center; */
+  margin-left: 40%;
+  margin-bottom: 0%;
+}
+
+</style>
---
+++ vue-admin/src/views/order/product/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <p class="font">海宁市中奇纸箱包装厂</p>
+    <p class="font2">生产 制造单</p>
+    <!-- <div id="app" style="padding:20px;">
+      <div class="panel panel-primary">
+        <div class="panel-heading">用户管理</div>
+        <table class="table table-bordered table-striped text-center">
+          <thead>
+            <tr>
+              <th>序号</th>
+              <th>用户名</th>
+              <th>年龄</th>
+              <th>毕业学校</th>
+              <th>操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user,index) in users">
+              <td>{{ index+1 }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.school }}</td>
+              <td><button @click="remove(index)">remove</button></td>
+            </tr>
+            <tr>
+              <td />
+              <td><input id="name" v-model="user.name" type="text"></td>
+              <td><input id="age" v-model="user.age"type="text"></td>
+              <td><input id="school" v-model="user.school"type="text"></td>
+              <td><button @click="insert">insert</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product'
+}
+</script>
+
+<style>
+.font{
+  font-weight: bold;
+  width:400px;
+  height: 100px;
+  line-height: 100px;
+  font-size: 30px;
+  font-family: 'Courier New', Courier, monospace;
+  /* text-align: center; */
+  margin-left: 40%;
+}
+.font2{
+  width:400px;
+  height: 100px;
+  line-height: 10px;
+  font-size: 30px;
+  font-family: 'Courier New', Courier, monospace;
+  /* text-align: center; */
+  margin-left: 45%;
+  font-weight: bold;
+}
+
+</style>
