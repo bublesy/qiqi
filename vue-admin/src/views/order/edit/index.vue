9aec2971a56fead71269ca386e919e72efb42e8d
柠檬心凉不心萌a <978276634@qq.com>
2020-09-10 14:16:47 +0800

    合并从 hc 到 master 新建基本资料文件夹
    https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton/merge_request/172785  
    
    squash 合并:
    
    * 新建基本资料文件夹

---
+++ vue-admin/src/views/order/edit/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div style="padding:10px">
+    <el-form ref="form" :model="form" label-width="80px" size="mini" :inline="true">
+      <el-form-item label="客户单号:">
+        <el-input v-model="form.no" />
+      </el-form-item>
+      <el-form-item label="款号:">
+        <el-input v-model="form.modelNo" />
+      </el-form-item>
+      <!-- xlk -->
+      <el-form-item label="箱型:">
+        <!-- <el-input v-model="form.boxType" /> -->
+        <el-select v-model="boxType" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>
+      <!-- xlk -->
+      <el-form-item label="供方:">
+        <!-- <el-input v-model="form.supplier" /> -->
+        <el-select v-model="supplier" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" size="mini">查询</el-button>
+    <el-button type="primary" size="mini">新增</el-button>
+    <el-card>
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="no" label="客户单号" width="120" />
+        <el-table-column prop="modelNo" label="款号" width="120" />
+        <el-table-column prop="boxType" label="箱型" width="120" />
+        <el-table-column prop="unit" label="单位" width="120" />
+        <el-table-column prop="material" label="材质" width="120" />
+        <el-table-column prop="orderNum" label="订单数量" width="120" />
+        <el-table-column prop="perPrice" label="单价" width="120" />
+        <el-table-column prop="money" label="金额" width="120" />
+        <el-table-column prop="supplier" label="供方" width="120" />
+        <el-table-column prop="createdTime" label="制单时间" width="120" />
+        <el-table-column prop="making" label="制单人" width="120" />
+        <el-table-column prop="auditTime" label="审核时间" width="120" />
+        <el-table-column prop="audit" label="审核人" width="120" />
+        <el-table-column label="操作" width="120">
+          <template slot-scope="scope">
+            <el-link @click="deleted(scope.row.id)">删除</el-link>
+            <el-link @click="updated(scope.row.id)">编辑</el-link>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        :current-page="queryForm.page"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="queryForm.size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-card>
+    <editDialog :dialog="editDialog" />
+  </div>
+</template>
+
+<script>
+import editDialog from '@/views/order/edit/edit'
+export default {
+  name: 'Edit',
+  components: { editDialog },
+  data() {
+    return {
+      total: 0,
+      queryForm: {
+        page: 1,
+        count: 10,
+        no: '',
+        modelNo: '',
+        boxType: '',
+        supplier: ''
+      },
+      form: {},
+      editDialog: {
+        show: false
+      },
+      tableData: [],
+      value: '',
+      options: []
+    }
+  },
+  created() {
+    var data = {
+      no: '22'
+    }
+    this.tableData.push(data)
+  },
+  methods: {
+    initTable() {},
+    handleSelectionChange() {},
+    handleSizeChange(size) {
+      this.queryForm.size = size
+      this.initTable()
+    },
+    handleCurrentChange(page) {
+      this.queryForm.page = page
+      this.initTable()
+    },
+    deleted(id) {},
+    updated(id) {
+      this.editDialog.show = true
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.el-link{
+  color: rgb(0, 162, 255);
+  font: 1em sans-serif;
+}
+
+</style>
---
+++ vue-admin/src/views/order/schedule/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div style="padding:30px">
+    <el-table
+      ref="multipleTable"
+      :data="tableData"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55" />
+      <el-table-column prop="schedule" label="排期日期" width="120" />
+      <el-table-column prop="taskNo" label="任务编号" width="120" />
+      <el-table-column prop="customerInfo" label="客户简介" width="120" />
+      <el-table-column prop="boxType" label="箱型" width="120" />
+      <el-table-column prop="material" label="材质" width="120" />
+      <el-table-column prop="stare" label="楞型" width="120" />
+      <el-table-column prop="cartonSize" label="纸箱尺寸" width="120" />
+      <el-table-column prop="paperLength" label="纸长" width="120" />
+      <el-table-column prop="paperWidth" label="纸宽" width="120" />
+      <el-table-column prop="pressureSpecification" label="分压规格" width="120" />
+      <el-table-column prop="unit" label="单位" width="120" />
+      <el-table-column prop="orderNum" label="订单数量" width="120" />
+      <el-table-column prop="productNum" label="成品数量" width="120" />
+      <el-table-column prop="delivery" label="交货日期" width="120" />
+      <el-table-column prop="isSchedule" label="是否排班" width="120" />
+    </el-table>
+    <el-pagination
+      :current-page="page"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [],
+      total: 0,
+      size: 10,
+      page: 1
+
+    }
+  },
+  methods: {
+    initTable() {},
+    handleSelectionChange() {},
+    handleSizeChange(size) {
+      this.size = size
+      this.initTable()
+    },
+    handleCurrentChange(page) {
+      this.page = page
+      this.initTable()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
