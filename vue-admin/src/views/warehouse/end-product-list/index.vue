cf2e2fe7d974c7cf7124e9b6afa87075eb89393f
tkless <accounts_5f041221b23b8d003696b26a@mail.teambition.com>
2020-09-13 16:59:05 +0800

    合并 #180167 从 sun 到 master 采购 仓库
    https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton/merge_request/180167  
    
    squash 合并:
    
    * 采购
    
    * Merge remote-tracking branch 'remotes/origin/master' into sun
    
    * 采购 仓库
    
    * 采购 仓库
    
    * a
    
    * Merge branch 'master' into sun
    
    # Conflicts:
    #	vue-admin/src/views/purchase/purchase-order-printing/index.vue
    #	vue-admin/src/views/purchase/purchase-order/index.vue
    
    * 合并 #179997 从 master 到 sun
    合并 #179995 从 sun 到 master a
    
    https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton/merge_request/179997  
    
    squash 合并:
    
    * 合并 #179995 从 sun 到 master
    a
    
    https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton/merge_request/179995  
    
    squash 合并:
    
    * 采购
    
    * Merge remote-tracking branch 'remotes/origin/master' into sun
    
    * 采购 仓库
    
    * 采购 仓库
    
    * a
    
    * Merge branch 'master' into sun
    
    # Conflicts:
    #	vue-admin/src/views/purchase/purchase-order-printing/index.vue
    #	vue-admin/src/views/purchase/purchase-order/index.vue
    
    * 采购 仓库
    
    * Merge remote-tracking branch 'remotes/origin/master' into sun
    
    # Conflicts:
    #	vue-admin/src/views/purchase/purchase-order/index.vue

--- vue-admin/src/views/warehouse/end-product-list/index.vue
+++ vue-admin/src/views/warehouse/end-product-list/index.vue
@@ -1,19 +1,23 @@
 <template>
   <el-container>
     <el-main>
-      <el-form :inline="true" :model="form" size="mini" align="center">
-        <el-form-item label="入仓单号:">
-          <el-input v-model="form.documentsNo" />
-        </el-form-item>
-        <el-form-item label="任务编号:">
-          <el-input v-model="form.taskNumber" />
-        </el-form-item>
+      <h1 align="center">成品仓库</h1>
+      <el-form :inline="true" :model="form" size="mini">
         <el-form-item label="开单人">
           <el-input v-model="form.drawer" />
         </el-form-item>
-        <el-button type="warning" size="mini">查询</el-button>
+
+        <el-form-item label="成品送货:">
+          <el-select v-model="form.carryTo" :clearable="true">
+            <el-option label="已送货" value="已送货" />
+            <el-option label="未送货" value="未送货" />
+          </el-select>
+        </el-form-item>
+
+        <el-button type="primary" size="mini">查询</el-button>
         <el-button type="primary" size="mini" @click="purAdd">新增</el-button>
-        <!-- <el-button type="danger" size="mini" @click="drop">删除</el-button> -->
+        <el-button type="warning" size="mini" @click="selectPrinting">选择打印</el-button>
+        <el-button type="warning" size="mini" @click="wholePrinting">整页打印</el-button>
       </el-form>
       <div>
         <el-table
@@ -21,25 +25,30 @@
           :data="tableData"
           highlight-current-row
           style="width: 100%"
-          @current-change="handleCurrentChange"
+          @selection-change="handleSelectionChange"
         >
+          <el-table-column type="selection" width="55" />
           <el-table-column v-show="true" prop="documentsNo" label="入仓单号" width="140" />
           <el-table-column v-show="true" prop="taskNumber" label="任务编号" width="140" />
           <el-table-column v-show="true" prop="customerName" label="客户名称" width="140" />
+          <el-table-column v-show="true" prop="serialName" label="物品名称" width="140" />
           <el-table-column v-show="true" prop="customerNo" label="客户单号" width="140" />
+          <el-table-column v-show="true" prop="orderQuantity" label="订单数量" width="140" />
+          <el-table-column v-show="true" prop="deliveryQuantity" label="送货数量" width="140" />
           <el-table-column v-show="true" prop="typeNo" label="款号" width="140" />
           <el-table-column v-show="true" prop="boxType" label="箱型" width="140" />
           <el-table-column v-show="true" prop="material" label="材质" width="140" />
           <el-table-column v-show="true" prop="length" label="长" width="140" />
           <el-table-column v-show="true" prop="width" label="宽" width="140" />
           <el-table-column v-show="true" prop="height" label="高" width="140" />
-          <el-table-column v-show="true" prop="unit" label="单位" width="140" />
+          <el-table-column v-show="true" prop="unitPrice" label="单价" width="140" />
           <el-table-column v-show="true" prop="EndProductPos" label="成品仓位" width="140" />
           <el-table-column v-show="true" prop="warehousingTime" label="入仓时间" width="140" />
-          <el-table-column label="操作" width="120">
+          <el-table-column label="操作" width="180">
             <template slot-scope="scope">
               <el-link type="danger" size="small" @click="drop(scope.row.id)">删除</el-link>
               <el-link type="primary" size="small" @click="modifyPur(scope.row.id)">编辑</el-link>
+              <el-link type="warning" size="small" @click="printing">生成送货单</el-link>
             </template>
           </el-table-column>
         </el-table>
@@ -56,18 +65,10 @@
         />
       </div>
       <!-- 新增/编辑纸板入仓单 -->
-      <el-dialog :title="titleType+'纸板入仓单'" :visible.sync="purAddVisible">
+      <el-dialog :title="titleType+'成品入仓单'" :visible.sync="purAddVisible">
+
         <el-form ref="purForm" :rules="purRules" :inline="true" :model="formAdd" size="mini" label-width="80px">
-          <el-form-item label="任务编号" prop="taskNumber">
-            <el-select v-model="formAdd.taskNumber">
-              <el-option
-                v-for="item in supplierFor"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              />
-            </el-select>
-          </el-form-item>
+
           <el-form-item label="入仓单号" prop="pricing">
             <el-input v-model="formAdd.documentsNo" disabled />
           </el-form-item>
@@ -82,140 +83,119 @@
               placeholder="选择日期"
             />
           </el-form-item>
+
+          <el-form-item label="客户名称">
+            <el-select v-model="formAdd.customerName" size="mini">
+              <el-option
+                v-for="item in customerFor"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="交货日期">
+            <el-select v-model="formAdd.deliveryDate" size="mini">
+              <el-option
+                v-for="item in deliveryDateFor"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="客户单号">
+            <el-select v-model="formAdd.customerNo" size="mini">
+              <el-option
+                v-for="item in customerNoFor"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="物品名称">
+            <el-select v-model="formAdd.serialName" size="mini">
+              <el-option
+                v-for="item in serialNameFor"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="规格">
+            <el-select v-model="formAdd.specifications" size="mini">
+              <el-option
+                v-for="item in specificationsFor"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="款号">
+            <el-input v-model="formAdd.typeNo" />
+          </el-form-item>
+
+          <el-form-item label="箱型">
+            <el-input v-model="formAdd.boxType" />
+          </el-form-item>
+
+          <el-form-item label="材质">
+            <el-input v-model="formAdd.material" />
+          </el-form-item>
+
+          <el-form-item label="订单数量">
+            <el-input v-model="formAdd.orderQuantity" />
+          </el-form-item>
+
+          <el-form-item label="送货数量">
+            <el-input v-model="formAdd.deliveryQuantity" />
+          </el-form-item>
+
+          <el-form-item label="长">
+            <el-input v-model="formAdd.length" />
+          </el-form-item>
+
+          <el-form-item label="宽">
+            <el-input v-model="formAdd.width" />
+          </el-form-item>
+
+          <el-form-item label="高">
+            <el-input v-model="formAdd.height" />
+          </el-form-item>
+
+          <el-form-item label="单价">
+            <el-input v-model="formAdd.unitPrice" />
+          </el-form-item>
+
+          <el-form-item label="成品仓位">
+            <el-input v-model="formAdd.EndProductPos" />
+          </el-form-item>
+
+          <el-form-item label="入仓时间" prop="warehousingTime">
+            <el-date-picker
+              v-model="formAdd.warehousingTime"
+              align="right"
+              type="date"
+              placeholder="选择日期"
+            />
+          </el-form-item>
+
           <el-form-item label="备注:">
             <el-input v-model="formAdd.remark" />
           </el-form-item>
         </el-form>
-        <el-button type="primary" size="mini" @click="addTaskNumber">添加任务编号</el-button>
-        <el-table
-          ref="multipleTable"
-          :data="addTableData"
-          tooltip-effect="dark"
-          style="width: 100%"
-          size="small"
-        >
-          <el-table-column label="操作">
-            <template slot-scope="scope">
-              <el-link class="el-icon-delete" @click="delRule(scope)" />
-            </template>
-          </el-table-column>
-          <el-table-column prop="taskNumber" label="任务编号" width="140">
-            <template slot-scope="scope">
-              <el-link type="primary" @click="modifyTask(scope)">{{ scope.row.taskNumber }}</el-link>
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="customerName" label="客户名称" width="140">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.customerName" size="mini" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="customerNo" label="客户单号" width="140">
-            <template slot-scope="scope">
-              <el-input v-model="scope.row.customerNo" size="mini" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="typeNo" label="款号" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.typeNo" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="boxType" label="箱型" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.boxType" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="material" label="材质" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.material" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="length" label="长" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.length" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="width" label="宽" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.width" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="height" label="高" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.height" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="unit" label="单位" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.unit" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="EndProductPos" label="成品仓位" width="140">
-            <template slot-scope="scope">
-              <el-input-number v-model="scope.row.EndProductPos" size="mini" :controls="false" />
-            </template>
-          </el-table-column>
-          <el-table-column v-show="true" prop="warehousingTime" label="入仓时间" width="140">
-            <template slot-scope="scope">
-              <el-date-picker
-                v-model="scope.row.warehousingTime"
-                align="right"
-                type="date"
-                size="mini"
-                placeholder="选择日期"
-              />
-            </template>
-          </el-table-column>
-        </el-table>
+
         <span slot="footer" class="dialog-footer">
           <el-button size="small" @click="purAddNo">取 消</el-button>
           <el-button size="small" type="primary" @click="purAddOk('purForm')">确 定</el-button>
-        </span>
-      </el-dialog>
-      <!-- 添加任务编号 -->
-      <el-dialog title="添加任务编号" :visible.sync="taskNumberVisible" width="800px">
-        <el-table
-          ref="multipleTable"
-          :data="taskNumberTable"
-          stripe
-          highlight-current-row
-          style="width: 100%"
-          border
-          height="300"
-          :row-key="getRowKeys"
-          :row-class-name="tableRowClassName"
-          @current-change="selectionChange"
-          @selection-change="selectionChange"
-        >
-          <el-table-column width="50px" align="center">
-            <template slot-scope="scope">
-              {{ scope.$index + 1 }}
-            </template>
-          </el-table-column>
-          <el-table-column type="selection" :reserve-selection="true" width="55" align="center" />
-          <el-table-column property="taskNumber" label="任务编号" />
-          <el-table-column property="customerName" label="客户名称" />
-          <el-table-column property="typeNo" label="款号" />
-          <el-table-column property="boxType" label="箱型" />
-          <el-table-column property="material" label="材质" />
-          <el-table-column property="length" label="长" />
-          <el-table-column property="width" label="宽" />
-          <el-table-column property="height" label="高" />
-          <el-table-column property="cardPickNumber" label="纸板领料数量" />
-          <el-table-column property="warehousingNumber" label="可入仓数量" />
-        </el-table>
-        <el-pagination
-          background
-          layout="total, sizes, prev, pager, next"
-          :total="pagination.total"
-          :current-page="pagination.page"
-          :page-size="pagination.size"
-          align="center"
-          @size-change="sizeChange"
-          @current-change="pageChange"
-        />
-        <span slot="footer" class="dialog-footer">
-          <el-button size="small" @click="taskNumberVisible = false">取 消</el-button>
-          <el-button size="small" type="primary" @click="selectedConfirm">确 定</el-button>
         </span>
       </el-dialog>
 
@@ -301,7 +281,6 @@ export default {
         name: '净宽'
       }],
       titleType: '',
-      taskNumberVisible: false,
       taskNumberTable: [{
         taskNumber: '1',
         taskName: '就这?'
@@ -309,19 +288,51 @@ export default {
         taskNumber: '2',
         taskName: '就这a ?'
       }],
-      selectedTableData: [],
       currentRowTow: {},
       modifyTaskVisible: false,
       modifyTaskTable: [{
         customerName: '张三',
         taskNumber: '2'
       }],
-      dropRow: {},
-      indexId: {}
+      multipleSelection: [],
+      indexId: {},
+      serialNameFor: [],
+      deliveryDateFor: [],
+      customerNoFor: [],
+      specificationsFor: []
 
     }
   },
   methods: {
+    // 生成送货单
+    printing() {
+      this.$router.push('/end_product_list_delivery_note')
+    },
+    // 整页打印
+    wholePrinting() {
+      if (this.form.carryTo === '已送货') {
+        this.$router.push('/end_product_list_delivery_note')
+      } else if (this.form.carryTo === '未送货') {
+        this.$router.push('/end_product_list_not_delivery_note')
+      } else {
+        this.$router.push('/end_product_list_delivery_note')
+      }
+    },
+    // 选择打印
+    selectPrinting() {
+      if (this.form.carryTo === '已送货') {
+        this.$router.push('/end_product_list_delivery_note')
+      } else if (this.form.carryTo === '未送货') {
+        this.$router.push('/end_product_list_not_delivery_note')
+      } else {
+        if (this.multipleSelection.length === 0) {
+          this.$message.error('请选择打印的内容！！！')
+          return
+        } else {
+          this.$router.push('/end_product_list_delivery_note')
+        }
+      }
+    },
     tableRowClassName({ row, rowIndex }) {
       if (row.consignment) {
         return 'green-row'
@@ -333,8 +344,8 @@ export default {
     getRowKeys(row) {
       return row.id
     },
-    handleCurrentChange(row) {
-      this.dropRow = row
+    handleSelectionChange(row) {
+      this.multipleSelection = row
     },
     // 删除
     drop() {
@@ -381,37 +392,6 @@ export default {
      */
     modifyTaskChange(val) {
       this.currentRowTow = val
-    },
-    /**
-     * 选中数据改变事件
-     */
-    selectionChange(val) {
-      this.selectedTableData = val
-    },
-    // 确定任务编号 回调
-    selectedConfirm() {
-      this.selectedTableData.forEach(x => {
-        var status = true
-        if (this.addTableData.length > 0) {
-          this.addTableData.forEach(y => {
-            if (x.taskNumber === y.taskNumber) {
-              status = false
-              this.$alert('任务编号【' + x.taskNumber + '】已存在，不能重复添加！', '提示', {
-                confirmButtonText: '确定',
-                callback: action => {}
-              })
-            }
-          })
-        }
-        if (status) {
-          this.addTableData.push(x)
-        }
-      })
-      this.taskNumberVisible = false
-    },
-    // 添加任务编号
-    addTaskNumber() {
-      this.taskNumberVisible = true
     },
     // 点击任务编号弹出弹窗
     modifyTask(scope) {
