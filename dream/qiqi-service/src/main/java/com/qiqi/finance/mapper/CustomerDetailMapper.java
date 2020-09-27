a1873b8b28604ee8a36f39d7bda963e6acfe09e7
18368673570 <419009863@qq.com>
2020-09-27 15:48:44 +0800

    aa

---
+++ dream/qiqi-service/src/main/java/com/qiqi/finance/mapper/CustomerDetailMapper.java
@@ -0,0 +1,16 @@
+package com.qiqi.finance.mapper;
+
+import com.qiqi.finance.entity.CustomerDetailDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author QiQiDream
+ * @since 2020-09-26
+ */
+public interface CustomerDetailMapper extends BaseMapper<CustomerDetailDO> {
+
+}
---
+++ dream/qiqi-service/src/main/java/com/qiqi/finance/service/impl/CustomerDetailServiceImpl.java
@@ -0,0 +1,20 @@
+package com.qiqi.finance.service.impl;
+
+import com.qiqi.finance.entity.CustomerDetailDO;
+import com.qiqi.finance.mapper.CustomerDetailMapper;
+import com.qiqi.finance.service.CustomerDetailService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author QiQiDream
+ * @since 2020-09-26
+ */
+@Service
+public class CustomerDetailServiceImpl extends ServiceImpl<CustomerDetailMapper, CustomerDetailDO> implements CustomerDetailService {
+
+}
