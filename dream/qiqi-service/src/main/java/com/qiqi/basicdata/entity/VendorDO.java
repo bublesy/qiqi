385b888e400b13cfb6aeb22127e6a8fff2ec59b2
蒋涛 <1271210707@qq.com>
2020-09-29 09:57:56 +0800

    a

---
+++ dream/qiqi-service/src/main/java/com/qiqi/basicdata/entity/VendorDO.java
@@ -0,0 +1,33 @@
+package com.qiqi.basicdata.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.qiqi.common.base.BaseModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author QiQiDream
+ * @since 2020-09-29
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("vendor")
+public class VendorDO extends BaseModel {
+
+    private static final long serialVersionUID=1L;
+
+    /**
+     * 厂商
+     */
+    private String name;
+
+    private Integer phone;
+
+    private String address;
+
+
+}
