105865e28b1139c6d9b3045097da5fd82f4dd61a
18368673570 <419009863@qq.com>
2020-09-30 14:04:15 +0800

    Merge branch 'master' of https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton
    # Conflicts:
    #	dream/admin/src/main/java/com/qiqi/admin/order/api/OrderController.java
    #	vue-admin/src/views/warehouse/cardboard-list/index.vue
    #	vue-admin/src/views/warehouse/end-product-list/index.vue

--- vue-admin/src/views/order/daily/print.vue
+++ vue-admin/src/views/order/daily/print.vue
@@ -3,6 +3,7 @@
     <el-button type="info" style="margin-left:84%" @click="back">返回</el-button>
     <el-button v-print="'#printTest'" type="success">打印</el-button>
     <el-card id="printTest">
+      <h1>{{ name2 }}</h1>
       <p class="font">客户订单日报表</p>
       <span style="margin-left:25%">制表:{{ name }}</span>
       <span style="margin-left:400px">打印日期:{{ now }}</span><hr>
@@ -44,6 +45,7 @@
 
 <script>
 // import { export2Excel } from '@/utils/common'
+import { list } from '@/api/basedata/firm'
 import { getUser } from '@/api/order/customerOrder'
 export default {
   name: 'Daily',
@@ -57,25 +59,36 @@ export default {
       //   date: null
       // },
       now: null,
-      name: ''
+      name: '',
+      name2: ''
     }
   },
   created() {
+    list().then(res => {
+      console.log(res)
+      var firmList = []
+      firmList = res.list
+      firmList.forEach(a => {
+        if (a.isFirm === true) {
+          this.name2 = a.name
+          console.log('name:' + this.name2)
+        }
+      })
+    })
     getUser().then(res => {
       this.name = res.nickname
     })
     var date = new Date()
     this.now = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-    var list = []
+    var list2 = []
     var object = this.$route.query
-    console.log(object)
     for (const key in object) {
       if (object.hasOwnProperty(key)) {
         const element = object[key]
-        list.push(element)
+        list2.push(element)
       }
     }
-    this.tableData = list
+    this.tableData = list2
   },
   methods: {
     back() {
