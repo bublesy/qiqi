a33b449b6d948208b234f88d86886cf6fd646d75
18368673570 <419009863@qq.com>
2020-10-12 08:59:21 +0800

    Merge branch 'master' of https://codeup.aliyun.com/5f03f1a199f4b36b1784639d/Other/Carton
    # Conflicts:
    #	dream/admin/src/main/java/com/qiqi/admin/order/api/OrderController.java
    #	vue-admin/src/views/warehouse/cardboard-list/index.vue
    #	vue-admin/src/views/warehouse/end-product-list/index.vue

---
+++ dream/qiqi-service/src/main/java/com/qiqi/warning/service/WarningService.java
@@ -0,0 +1,16 @@
+package com.qiqi.warning.service;
+
+import com.qiqi.warning.entity.WarningDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 预警表 服务类
+ * </p>
+ *
+ * @author QiQiDream
+ * @since 2020-10-12
+ */
+public interface WarningService extends IService<WarningDO> {
+
+}
---
+++ dream/qiqi-service/src/main/java/com/qiqi/warning/service/impl/WarningServiceImpl.java
@@ -0,0 +1,20 @@
+package com.qiqi.warning.service.impl;
+
+import com.qiqi.warning.entity.WarningDO;
+import com.qiqi.warning.mapper.WarningMapper;
+import com.qiqi.warning.service.WarningService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 预警表 服务实现类
+ * </p>
+ *
+ * @author QiQiDream
+ * @since 2020-10-12
+ */
+@Service
+public class WarningServiceImpl extends ServiceImpl<WarningMapper, WarningDO> implements WarningService {
+
+}
